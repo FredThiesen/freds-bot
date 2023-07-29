98eb82facc1cd5a75d6f16690f88063fe730d645
Fred <ricardofredt@hotmail.com>
2023-07-29 20:25:40 -0300

    fix almanax last day of month

--- scripts/getAlmanax.ts
+++ scripts/getAlmanax.ts
@@ -2,16 +2,18 @@ import { Moment } from "moment"
 const almanax = require("../assets/almanax.json")
 import { Almanax } from "../interfaces/Almanax"
 
-export const getAlmanax = (date: Moment): Almanax => {
+export const getAlmanax = (date: Moment): Almanax | null => {
 	//convert from brazil to france dateTime
 	date.add(5, "hours")
 
 	const monthNumber = date.month() + 1
 	const dayNumber = date.date()
 
+	console.log(monthNumber, dayNumber)
+
 	const almanaxDay = almanax[monthNumber][dayNumber] || null
 
-	if (!almanaxDay) throw new Error("Almanax day not found")
+	if (!almanaxDay) return null
 
 	return almanaxDay
 }
